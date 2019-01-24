8f5a9464d3c331ef954bf2da1fd2ad2219083988
Clayton Sweeten <cjsweeten101@gmail.com>
2019-01-24 15:49:48 -0700

    Player movement, right and left

--- Player/Player.gd
+++ Player/Player.gd
@@ -1,15 +1,25 @@
 extends KinematicBody2D
 
-# class member variables go here, for example:
-# var a = 2
-# var b = "textvar"
+var UP = Vector2(0,-1)
+var gravity = Vector2(0,25)
+var acceleration = 200
+var max_speed = 700
+var ground_friction = .3
+var current_speed = Vector2(0,0)
 
-func _ready():
-	# Called when the node is added to the scene for the first time.
-	# Initialization here
-	pass
+func _physics_process(delta):
+	move()
 
-#func _process(delta):
-#	# Called every frame. Delta is time since last frame.
-#	# Update game logic here.
-#	pass
+func move():
+
+	if Input.is_action_pressed("right"):
+		current_speed.x = min(current_speed.x + acceleration, max_speed)
+	elif Input.is_action_pressed("left"):
+		current_speed.x = max(current_speed.x - acceleration, -max_speed)
+	
+	if is_on_floor():
+		current_speed.x = lerp(current_speed.x, 0, ground_friction)
+	else:
+		current_speed += gravity
+	
+	current_speed = move_and_slide(current_speed, UP)
