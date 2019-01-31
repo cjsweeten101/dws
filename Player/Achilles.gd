2f017927f2bc7c976dfb3a4de8133e61a2d80dfc
Clayton Sweeten <cjsweeten101@gmail.com>
2019-01-31 15:44:35 -0700

    launch spear

--- Player/Achilles.gd
+++ Player/Achilles.gd
@@ -7,12 +7,17 @@ var max_speed = 700
 var current_speed = Vector2()
 var ground_friction = .3
 var direction
+var strange_aim_scaling_value = 89
+var can_grapple = true
+var spear
 
 func ready():
 	current_speed = move_and_slide(gravity, UP)
 
 func _physics_process(delta):
 	move()
+	update_aim()
+	grapple()
 
 func move():
 	var friction = false
@@ -30,3 +35,22 @@ func move():
 	
 	if friction == true:
 		current_speed.x = lerp(current_speed.x, 0, ground_friction)
+
+func update_aim():
+	if current_speed.x > 0:
+		$AimSprite.rotation_degrees = current_speed.x/max_speed*strange_aim_scaling_value
+	else:
+		$AimSprite.rotation_degrees = 0
+
+func grapple():
+	if Input.is_action_just_pressed("action") and can_grapple:
+		deploy_spear()
+	elif Input.is_action_just_released("action"):
+		spear.retract()
+
+func deploy_spear():
+	spear = load("res://Player/Spear.tscn")
+	spear = spear.instance()
+	get_parent().add_child(spear)
+	spear.global_position = global_position
+	spear.deploy($AimSprite.rotation_degrees)
--- Player/Spear.gd
+++ Player/Spear.gd
@@ -3,13 +3,54 @@ extends Node2D
 # class member variables go here, for example:
 # var a = 2
 # var b = "textvar"
+var deployed = false
+var current_speed = Vector2()
+var UP = Vector2(0,-1)
+var gravity = Vector2(0,40)
+var air_friction = .3
+var launch_speed = 1000
+var frozen = false
+var retract = false
 
 func _ready():
-	# Called when the node is added to the scene for the first time.
 	# Initialization here
 	pass
 
-#func _process(delta):
-#	# Called every frame. Delta is time since last frame.
-#	# Update game logic here.
-#	pass
+func _process(delta):
+	if deployed and !frozen:
+		update_angle()
+		gravity()
+		current_speed = move_and_slide(current_speed, UP)
+	elif frozen:
+		current_speed = Vector2(0,0)
+		if retract:
+			queue_free()
+	
+	current_speed = move_and_slide(current_speed, UP)
+
+func deploy(angle):
+	deployed = true
+	var angle_vect = Vector2(0,-1).rotated(angle*PI/180)
+	current_speed = angle_vect*launch_speed
+
+func gravity():
+	current_speed += gravity
+
+func retract():
+	retract = true
+
+func update_angle():
+	if !current_speed.y == 0:
+		rotation = current_speed.angle() + PI/2
+	
+
+func _on_HitBox_area_entered(area):
+	print(area)
+
+
+func _on_HitBox_body_entered(body):
+	if !body.is_in_group("player"):
+		freeze()
+
+func freeze():
+	frozen = true
