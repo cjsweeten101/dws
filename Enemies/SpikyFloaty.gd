905fa02bca5e32cec0bb64e94a55399f2f91c04c
Clayton Sweeten <cjsweeten101@gmail.com>
2019-02-13 11:59:38 -0700

    spiky avoids platforms

--- Enemies/SpikyFloaty.gd
+++ Enemies/SpikyFloaty.gd
@@ -3,10 +3,12 @@ extends KinematicBody2D
 # class member variables go here, for example:
 # var a = 2
 # var b = "textvar"
-var max_speed = 250
+var max_speed = 300
 var current_speed = Vector2(0,0)
 var target = Vector2()
 var last_direction = Vector2(0,0)
+var acceleration = 75
+var proximity_tile_pos
 
 #Need this boy to roam around randomly.
 #Basic Implemantion with a timer?
@@ -16,7 +18,8 @@ var last_direction = Vector2(0,0)
 #New direction could be restricted to a range based on last direction
 #So it doesn't appear so random.
 #also dont want him roaming too near to the ground or of screen. or on platforms
-
+#So maybe he has an area that detects platforms?  Doesn't pick values near them
+#acceleration/friction would be nice also
 func _ready():
 	randomize()
 
@@ -26,30 +29,46 @@ func _physics_process(delta):
 func move():
 	if $PauseTimer.is_stopped():
 		if $MoveTimer.is_stopped():
-			print("startin move")
-			current_speed = start_move()
+			var dir = new_dir()
+			if dir.x > 0:
+				current_speed.x = min(current_speed.x + acceleration, max_speed)
+			elif dir.x < 0:
+				current_speed.x = max(current_speed.x - acceleration, -max_speed)
+			if dir.y > 0:
+				current_speed.y = min(current_speed.y + acceleration, max_speed)
+			elif dir.y < 0:
+				current_speed.y = max(current_speed.y - acceleration, -max_speed)
+			if $MoveTimer.is_stopped():
+				$MoveTimer.start()
+	else:
+		current_speed.x = lerp(current_speed.x, 0, .1)
+		current_speed.y = lerp(current_speed.y, 0, .1)
+
+	proximity_check()
 	move_and_slide(current_speed)
 
-func start_move():
+func proximity_check():
+	if proximity_tile_pos != null:
+		$MoveTimer.stop()
+		$PauseTimer.start()
+		proximity_tile_pos = null
+
+func new_dir():
 	var rand_dir = Vector2()
 	if last_direction == Vector2(0,0):
 		rand_dir.x = rand_range(-1,1)
 		rand_dir.y = rand_range(-1,1)
 	else:
-		#Weight with current direction
+		#TODO weight with current direction
 		rand_dir.x = rand_range(-1,1)
 		rand_dir.y = rand_range(-1,1)
 
-	if $MoveTimer.is_stopped():
-		$MoveTimer.start()
-
 	last_direction = rand_dir
-	return max_speed*rand_dir
+	return rand_dir
 	
 func _on_MoveTimer_timeout():
 	$MoveTimer.stop()
-	$MoveTimer.wait_time = rand_range(.2,1)
-	current_speed = Vector2(0,0)
+	$MoveTimer.wait_time = rand_range(.5,1.5)
 	if $PauseTimer.is_stopped():
 		$PauseTimer.start()
 
@@ -57,3 +76,8 @@ func _on_MoveTimer_timeout():
 func _on_PauseTimer_timeout():
 	$PauseTimer.stop()
 	$PauseTimer.wait_time = rand_range(.5,1)
+
+
+func _on_Area2D_body_entered(body):
+	if body.is_in_group("tiles"):
+		proximity_tile_pos = body.global_position
