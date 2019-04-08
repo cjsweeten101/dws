8ca6a7fddc526c49dc080c4fb51e88a317d0f25a
Clayton Sweeten <cjsweeten101@gmail.com>
2019-04-08 12:55:59 -0600

    empty abstract enemy class (note need how to learn classes in godot)

---
+++ AbstractEnemy.gd
@@ -0,0 +1,9 @@
+extends KinematicBody2D
+
+#Ok so, in this class I should have:
+#Functions for attacking, checking if self is soft/spiky
+#Functions for getting hit/destroyed with anim
+#Empty body/collision shape
+#Empty sprite
+
+#Thats it I thiiiiiink
